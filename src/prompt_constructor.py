ae94433c34e8a3677fb16e2623c49552e1308e01
hzxwonder <2354693781@qq.com>
2025-02-21 16:10:25 +0800

    update

--- src/prompt_constructor.py
+++ src/prompt_constructor.py
@@ -51,7 +51,7 @@ def get_arch_definition(arch_src):
 # PROBLEM_STATEMENT = """You are an expert in CUDA programming and performance optimization. You write custom CUDA kernels to replace the pytorch operators in the given architecture to get speedups. \n
 #     You have complete freedom to choose the set of operators you want to replace. You may make the decision to replace some operators with custom CUDA kernels and leave others unchanged. You may replace multiple operators with custom implementations, consider operator fusion opportunities (combining multiple operators into a single kernel, for example, combining matmul+relu), or algorithmic changes (such as online softmax). You are only limited by your imagination.\n
 # """
-PROBLEM_STATEMENT = """You are an expert in CUDA programming and performance optimization. Please write custom CUDA kernels to replace the pytorch operators in the given architecture to get speedups on H100 GPU. \nYou are only limited by your imagination.\n
+PROBLEM_STATEMENT = """You are an expert in CUDA programming and performance optimization. Please write custom CUDA kernels to replace the pytorch operators in the given architecture to get speedups on 80G H100 GPU. \nYou are only limited by your imagination.\n
 """
 # PROBLEM_INSTRUCTION = """
 # Optimize the architecture named Model with custom CUDA operators! Name your optimized output architecture ModelNew. Output the new code in codeblocks. Please generate real code, NOT pseudocode, make sure the code compiles and is fully functional. Just output the new model code, no other text, and NO testing code! \n
@@ -128,10 +128,10 @@ def prompt_generate_custom_cuda_reflection(
     ```
     """
 
-    prompt += f"""
-    Here's the information of H100 GPU: 
-    {H100_description}
-    """
+    # prompt += f"""
+    # Here's the information of H100 GPU: 
+    # {H100_description}
+    # """
     if hist_results:
         if best_hist_flag:
             improvement_prompt = f"Below is the previously generated CUDA kernel code:\n"
